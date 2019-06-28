2ebf6be3ca88102ed087f27adc9108f3728519b8
Karolina Płóciennik <209434@edu.p.lodz.pl>
2019-06-28 02:13:36 +0200

    tests for addLike to post by differ user status

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -3,14 +3,27 @@ package edu.iis.mto.blog.rest.test;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
-import org.json.JSONObject;
 import org.junit.Test;
 
 public class LikePostTest extends FunctionalTests {
     private static final String ADD_LIKE_TO_POST_API = "/blog/user/{userId}/like/{postId}";
 
     @Test
-    public void postShouldReturnStatusConflict_IfUserHasStatusRemoved() {
+    public void postShouldReturnStatusFORBBIDEN_IfUserHasStatusRemoved() {
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_FORBIDDEN)
+                   .when()
+                   .post(ADD_LIKE_TO_POST_API, 3, 1);
+    }
+
+
+    @Test
+    public void postShouldReturnStatusFORBIDDEN_IfUserHasStatusNew() {
         RestAssured.given()
                    .accept(ContentType.JSON)
                    .header("Content-Type", "application/json;charset=UTF-8")
@@ -20,5 +33,20 @@ public class LikePostTest extends FunctionalTests {
                    .statusCode(HttpStatus.SC_FORBIDDEN)
                    .when()
                    .post(ADD_LIKE_TO_POST_API, 2, 1);
+    }
+
+
+
+    @Test
+    public void postShouldReturnStatusOK_IfUserHasStatusConfirmed() {
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_FORBIDDEN)
+                   .when()
+                   .post(ADD_LIKE_TO_POST_API, 1, 1);
     }
 }
